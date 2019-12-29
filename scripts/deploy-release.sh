a35a1e3e6d02d6a2673192dd4a4f2166c2e940b9
Muspah <simondw@sapo.pt>
2019-12-29 23:07:58 +0000

    Update POM, Travis

---
+++ scripts/deploy-release.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
+
+  echo -e "Running release script...\n"
+  echo -e "Publishing javadocs and artifacts...\n"
+  cd $HOME
+
+  rsync -r --quiet -e "ssh -p 2222 -o StrictHostKeyChecking=no" $HOME/build/internetpolice-eu/RandomTP/target/mvn-repo/ \
+  travis@travis.internetpolice.eu:WWW/repo/
+
+  echo -e "Publishing final plugin release...\n"
+
+  rsync -r --quiet -e "ssh -p 2222 -o StrictHostKeyChecking=no" $HOME/build/internetpolice-eu/RandomTP/target/RandomTP-*.jar \
+  travis@travis.internetpolice.eu:WWW/downloads/RandomTP/
+
+fi
